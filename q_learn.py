7d22628511279ba8ec4af05cc86d18d1496edbaf
Vitalii Ananev <vananiev@users.noreply.github.com>
2025-10-04 01:39:45 +0300

    fix learning formulas

--- q_learn.py
+++ q_learn.py
@@ -3,22 +3,21 @@
 #
 import time
 import numpy as np
-from tic_tac_toe_board import TicTacToeBoard
+from tic_tac_toe_board import TicTacToeBoard as board
 
 start_time_ns = time.time_ns()
 
 # Learning parameters
-alpha = 0.1  # learning rage
-gamma = 0.99 # discount factor
-epsilon = 0.1  # 10% steps are random
 num_episodes = 100_000
+avg_num_episodes_per_state = float(num_episodes) / board.state_cnt
+alpha = 1 / avg_num_episodes_per_state  # learning rage (use 1/N value, where each state episode makes an equal contribution)
+gamma = 1  # discount factor, 0 - for ignoring future benefits, 1 - for future result estimation
+epsilon = 1  # 0 - the world is not studied, 1 - studying moves (each action receives equal chances for estimation)
 
 # Learning
-state_cnt = pow(3, 9)  # 3 positions (X, O, empty) for 9 cells
-action_cnt = 9
-Q = np.zeros((state_cnt, action_cnt))
+Q = [[[0.0 for _ in range(board.action_cnt)] for _ in range(board.state_cnt)] for _ in range(board.player_cnt)]
+env = board()
 
-env = TicTacToeBoard()
 for episode in range(num_episodes):
     if episode % 100_000 == 0: print(f'Learning ({episode:,} episodes done)')
     state = env.reset()
@@ -34,37 +33,46 @@ for episode in range(num_episodes):
         else:
             # action with max Q-value
             action = actions[np.random.randint(0, len(actions))]  # if all actions have same value select random
-            max_q = Q[state][action]
+            max_q = Q[player][state][action]
             for i in actions:
-                q = Q[state][i]
+                q = Q[player][state][i]
                 if q > max_q:
                     max_q = q
                     action = i
 
         next_state, reward, done = env.do_action(player, action)
-        Q[state, action] = (1 - alpha) * Q[state, action] + alpha * (reward + gamma * np.max(Q[next_state, :]))
+        next_player = 0 if player == 1 else 1
+        # -max(Q[next_player][next_state]) - negative sign is used, because:
+        # the benefits of the next player are the penalty of the current player
+        Q[player][state][action] = ((1 - alpha) * Q[player][state][action] +
+                                    alpha * (reward + gamma * -max(Q[next_player][next_state])))
 
         state = next_state
-        player = 2 if player == 1 else 1
+        player = next_player
 
 time_spent_s = (time.time_ns() - start_time_ns) / 1_000_000_000
 print(f'Learning ({num_episodes:,} episodes done)')
 
 # Write to file
-print()
-print("Writing to 'q-table.csv'")
-cnt = 0
-with open('q-table.csv', 'w', newline='') as file:
-    file.write(
-        'State,      Action 1,  Action 2,  Action 3,  Action 4,  Action 5,  Action 6,  Action 7,  Action 8,  Action 9')
-    for state in range(0, state_cnt):
-        if sum(Q[state]) != 0:
-            cnt += 1
-            file.write('\n')
-            file.write(TicTacToeBoard.state_name(state))
-            for q in Q[state]:
-                file.write(', ')
-                file.write(f'{q:+.6f}')
+for player in range(2):
+    cnt = 0
+    player_name = 'o' if player == 0 else 'x'
+    file_name = f'q-table-{player_name}.csv'
+    with open(file_name, 'w', newline='') as file:
+        file.write(
+            'State,        Action 1,  Action 2,  Action 3,  Action 4,  Action 5,  Action 6,  Action 7,  Action 8,  Action 9')
+        for state in range(0, board.state_cnt):
+            if sum(Q[player][state]) != 0:
+                cnt += 1
+                file.write('\n')
+                name = board.state_name(state)
+                name = name[:3] + ' ' + name[3:6] + ' ' + name[6:]
+                file.write(name)
+                for q in Q[player][state]:
+                    file.write(', ')
+                    q_str = '    -    ' if q == 0.0 else f'{q:-.6f}'
+                    file.write(q_str.rjust(9))
+    print(f"\nTotal states for '{player_name}' player: " + str(cnt))
+    print(f"Written to '{file_name}'")
 
-print('Total states: ' + str(cnt))
-print('Spent time: ' + f'{time_spent_s:.3f} s')
+print('\nSpent time: ' + f'{time_spent_s:.3f} s')
--- tic_tac_toe_board.py
+++ tic_tac_toe_board.py
@@ -6,80 +6,100 @@ class TicTacToeBoard:
         [0, 1, 2],  # 1-st row
         [3, 4, 5],  # 2-nd row
         [6, 7, 8],  # 3-d  row
-        [0, 2, 6],  # 1-st col
+        [0, 3, 6],  # 1-st col
         [1, 4, 7],  # 2-nd col
         [2, 5, 8],  # 3-d  col
         [0, 4, 8],  # 1-st diagonal
         [2, 4, 6],  # 2-nd diagonal
     ]
+    player_cnt = 2
+    action_cnt = 9
+    state_cnt = pow(3, 9)  # 3 positions (X, O, empty) for 9 cells
     penalty = -1
-    reward = 1  # should be greater than penalty, don't use 0, this value in q-table used for unresolved move
+    # Don't use lower value. It's negate earlier moves if they can lead to win.
+    # Don't use higher value. This rewards the condition, leading to the opponent's winning position.
+    penalty_for_opponent_pre_winning_state = penalty
+    # Should be greater than penalty.
+    # Don't use 0, this value in q-table used for unresolved move.
+    # The reward should compensate for the entire penalty: (penalty * max_moves_cnt + reward) == 0
+    reward = abs(penalty) * action_cnt
 
     def __init__(self):
-        self.board = np.zeros(9, dtype=int)
+        self.board = np.full(TicTacToeBoard.action_cnt, fill_value=-1, dtype=int)
 
     def reset(self):
-        self.board = np.zeros(9, dtype=int)  # 3x3 board values: 0 - for empty, 1 - for 'X', '2' - for 'O'
+        self.board = np.full(TicTacToeBoard.action_cnt, fill_value=-1, dtype=int)  # 3x3 board values: -1 - for empty, '0' - for 'O', 1 - for 'X'
         return self.state()
 
     def state(self):
         state = 0
         for i in range(9):
-            state += self.board[i] * int(pow(3, 8 - i))
+            state += (self.board[i] + 1) * int(pow(3, 8 - i))
         return state
 
     @staticmethod
     def state_name(state):
         """
         :return: board description in 9 chars string.
-        String char index corresponds to cell index and value ane of 'X', 'O', 'B' for blank
+        String char index corresponds to cell index and value ane of 'x', 'o', '-' for blank
         """
         result = ""
         while state > 0:
             remainder = state % 3
             if remainder == 0:
-                result = 'b' + result
+                result = '-' + result
             elif remainder == 1:
-                result = 'x' + result
-            else:
                 result = 'o' + result
+            else:
+                result = 'x' + result
             state //= 3  # Integer division
-        return result.rjust(9, 'b')
+        return result.rjust(9, '-')
 
     def allowed_actions(self):
-        return [i for i in range(9) if self.board[i] == 0]
+        return [i for i in range(9) if self.board[i] == -1]
 
     def do_action(self, player, action):
         """
         Sets X or O symbol on board
-        :param player: 1 for 'X' player, 2 for 'O' player
+        :param player: 0 for 'O' player, 1 for 'X' player
         :param action: index of cell 0...8
         :return: agent reward
         """
+        # check is other player wins if moves to this position
+        other_player = 0 if player == 1 else 0
+        self.board[action] = other_player
+        possible_winner = self.who_wins()
+        # do action
         self.board[action] = player
         winner = self.who_wins()
-        reward = TicTacToeBoard.reward if winner == player else TicTacToeBoard.penalty
-        done = winner != 0
+        if winner == player:
+            reward = TicTacToeBoard.reward
+        elif possible_winner == other_player:
+            reward = TicTacToeBoard.penalty_for_opponent_pre_winning_state
+        else:
+            reward = TicTacToeBoard.penalty
+        done = winner != -1
         return self.state(), reward, done
 
     def who_wins(self):
         """
-        :return: 0 - no winner, 1 - 'X' player wins, 2 - 'O' player wins
+        :return: -1 - no winner, 0 - 'O' player wins, 1 - 'X' player wins
         """
         for cells in TicTacToeBoard.win_cells:
-            result = self.sum_not_empty_cells(cells)
-            if result == 3:
+            o_win = True
+            x_win = True
+            for cell in cells:
+                v = self.board[cell]
+                if v == -1:
+                    o_win = False
+                    x_win = False
+                    break
+                elif v == 1:
+                    o_win = False
+                elif v == 0:
+                    x_win = False
+            if o_win:
+                return 0
+            elif x_win:
                 return 1
-            elif result == 6:
-                return 2
-        return 0
-
-    def sum_not_empty_cells(self, cells):
-        sum = 0
-        for i in cells:
-            c = self.board[i]
-            if c == 0:
-                return -1  # cell is empty
-            else:
-                sum += c
-        return sum
+        return -1
