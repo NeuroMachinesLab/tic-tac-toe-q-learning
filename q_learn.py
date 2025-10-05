fix max(Q(next_state)) calculation

q_learn.py
@@ -8,31 +8,38 @@ from tic_tac_toe_board import TicTacToeBoard as board
 start_time_ns = time.time_ns()
 
 # Learning parameters
-num_episodes = 100_000
-avg_num_episodes_per_state = float(num_episodes) / board.state_cnt
-alpha = 1 / avg_num_episodes_per_state  # learning rage (use 1/N value, where each state episode makes an equal contribution)
-gamma = 1  # discount factor, 0 - for ignoring future benefits, 1 - for future result estimation
-epsilon = 1  # 0 - the world is not studied, 1 - studying moves (each action receives equal chances for estimation)
+num_episodes = 100_0000
+epsilon = 1.0  # 0 - the world is not studied, 1 - studying moves (each action receives equal chances for estimation)
+#avg_num_episodes_per_state = float(num_episodes) / board.state_cnt
+#alpha = 1.0 / avg_num_episodes_per_state  # learning rage (use 1/N value, where each state episode makes an equal contribution)
+#gamma = 1.0  # discount factor, 0 - for ignoring future benefits, 1 - for future result estimation
 
 # Learning
-Q = [[[0.0 for _ in range(board.action_cnt)] for _ in range(board.state_cnt)] for _ in range(board.player_cnt)]
+Q = np.zeros((board.player_cnt, board.state_cnt, board.action_cnt), dtype=np.longdouble)
 env = board()
 
 for episode in range(num_episodes):
     if episode % 100_000 == 0: print(f'Learning ({episode:,} episodes done)')
+    # The greater episode value, the more accurate Q table, so the prediction is more accurate.
+    # Train network with greater learning rate (alpha) and discount factor (gamma)
+    alpha = min(0.9, episode / num_episodes)
+    gamma = alpha
+
     state = env.reset()
     player = 1
     done = False
     while not done:
+        # Select next action
         actions = env.allowed_actions()
         if len(actions) == 0:
             break  # no one wins
         if np.random.rand() < epsilon:
-            i = np.random.randint(0, len(actions))  # random action
+            i = np.random.randint(0, len(actions))  # random action, studying move
             action = actions[i]
         else:
-            # action with max Q-value
-            action = actions[np.random.randint(0, len(actions))]  # if all actions have same value select random
+            # Action with max Q-value, the world is not studied
+            # Start with random action for case when all actions have same q-value
+            action = actions[np.random.randint(0, len(actions))]
             max_q = Q[player][state][action]
             for i in actions:
                 q = Q[player][state][i]
@@ -40,12 +47,20 @@ for episode in range(num_episodes):
                     max_q = q
                     action = i
 
+        # Update Q table
         next_state, reward, done = env.do_action(player, action)
+        # Finds max(Q(next_state))
+        # Don't use max_Q_next_state = 0. It is incorrect for case when all other values is negative
+        max_Q_next_state = float("-inf")
         next_player = 0 if player == 1 else 1
-        # -max(Q[next_player][next_state]) - negative sign is used, because:
+        for q in Q[next_player][next_state]:
+            if q > max_Q_next_state and q != 0.0:  # 0.0 is for unknown value, don't use it
+                max_Q_next_state = q
+        if max_Q_next_state == float("-inf"):
+            max_Q_next_state = 0  # max(Q(next_state)) is unknown, use 0
+        # Use negative sign for max(Q(next_state)), because:
         # the benefits of the next player are the penalty of the current player
-        Q[player][state][action] = ((1 - alpha) * Q[player][state][action] +
-                                    alpha * (reward + gamma * -max(Q[next_player][next_state])))
+        Q[player][state][action] = (1 - alpha) * Q[player][state][action] + alpha * (reward + gamma * -max_Q_next_state)
 
         state = next_state
         player = next_player